4cbe43ae50fa48293fc4875700c149ef46596e28
MKR-WORLD <manish.rewalliya@gmail.com>
2018-09-24 12:41:18 +0530

    Remove Feedback From Promotion Code

--- app/src/main/java/com/mkrworld/androidlibui/utils/MKRPromotion.kt
+++ app/src/main/java/com/mkrworld/androidlibui/utils/MKRPromotion.kt
@@ -43,90 +43,59 @@ class MKRPromotion {
          *
          * @param context
          */
-        fun getMoreApps(context: Context) {
+        fun openMoreAppScreen(context: Context) {
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse("market://search?q=pub:MKR WORLD"))
             try {
                 context.startActivity(intent)
             } catch (e: Exception) {
                 e.printStackTrace()
             }
-
         }
 
         /**
-         * Method top send feedback
-         *
-         * @param context
-         * @param appName
-         */
-        fun sendFeedback(context: Context, appName: String) {
-            try {
-                val emailIntent = Intent(Intent.ACTION_SEND)
-                emailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("mkrworldapps@gmail.com"))
-                emailIntent.putExtra(Intent.EXTRA_SUBJECT, appName)
-                emailIntent.putExtra(Intent.EXTRA_TEXT, "")
-                emailIntent.type = "message/rfc822"
-                emailIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                context.startActivity(emailIntent)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-
-        }
-
-        /**
-         * Method to share application
-         *
-         * @param context
-         * @param appName
-         * @param shareMessage
-         */
-        @JvmOverloads
-        fun shareApp(context: Context, appName: String, shareMessage: String) {
-            shareApp(context, appName, shareMessage, "")
-        }
-
-        /**
-         * Method to share application
-         *
-         * @param context
-         * @param appName
-         * @param shareMessage
-         * @param referrer
-         */
-        @JvmOverloads
-        fun shareApp(context: Context, appName: String, shareMessage: String, referrer: String) {
-            shareApp(context, appName, shareMessage, "", "http://play.google.com/store/apps/details?id=" + context.packageName, referrer)
-        }
-
-        /**
-         * Method to share application
-         *
-         * @param context
+         * Method to get the share Intent
+         * @see {intent = Intent(Intent.ACTION_SEND)}
+         * @see {emailIntent.putExtra(Intent.EXTRA_SUBJECT, appName)}
+         * @see {emailIntent.putExtra(Intent.EXTRA_TEXT, "")}
+         * @see {emailIntent.type = "text / *"}
+         * @see {emailIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)}
          * @param appName
          * @param shareMessage
          * @param shareUrl
-         * @param referrer
          */
         @JvmOverloads
-        fun shareApp(context: Context, appName: String, shareMessage: String, infoText: String, shareUrl: String, referrer: String) {
-            var shareUrl: String = shareUrl + if (referrer != null && !referrer.trim().isEmpty()) {
+        fun getShareIntent(appName: String, shareMessage: String, shareUrl: String): Intent {
+            val message = shareMessage + "\n\n" + shareUrl
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_SUBJECT, appName)
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            val sendIntent = Intent.createChooser(intent, "Share File")
+            sendIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            return intent
+        }
+
+        /**
+         * Method to get the URL used at the Time of Sharing App
+         * @param context
+         * @return http://play.google.com/store/apps/details?id=pkg
+         */
+        fun getShareAppUrl(context: Context): String {
+            return "http://play.google.com/store/apps/details?id=${context.packageName}"
+        }
+
+        /**
+         * Method to get the URL used at the Time of Sharing App
+         * @param context
+         * @param referrer
+         * @return http://play.google.com/store/apps/details?id=pkg&referrer=ref
+         */
+        fun getShareAppUrl(context: Context, referrer: String): String {
+            return getShareAppUrl(context) + if (referrer != null && !referrer.trim().isEmpty()) {
                 "&referrer=$referrer"
             } else {
                 ""
             }
-            val message = shareMessage + "\n\n\n\n" + shareUrl + if (infoText != null && !infoText.trim { it <= ' ' }.isEmpty()) {
-                "\n\n\n" + infoText
-            } else {
-                ""
-            }
-            val sentIntent = Intent(Intent.ACTION_SEND)
-            sentIntent.type = "text/plain"
-            sentIntent.putExtra(Intent.EXTRA_SUBJECT, appName)
-            sentIntent.putExtra(Intent.EXTRA_TEXT, message)
-            val sendIntent = Intent.createChooser(sentIntent, "Share File")
-            sendIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-            context.startActivity(sendIntent)
         }
     }
 }
