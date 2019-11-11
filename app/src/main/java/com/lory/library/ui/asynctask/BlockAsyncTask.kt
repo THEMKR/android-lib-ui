21bc99cefd8544cd3d3247a59e73d367d6fba63c
THEMKR <manish.rewalliya@gmail.com>
2019-11-11 17:08:54 +0530

    Add progresss update FLOW

--- app/src/main/java/com/lory/library/ui/asynctask/BlockAsyncTask.kt
+++ app/src/main/java/com/lory/library/ui/asynctask/BlockAsyncTask.kt
@@ -8,6 +8,7 @@ import android.content.Context
 abstract class BaseBlockAsyncTask<MKR, PROGRESS> : BaseAsyncTask<MKR, PROGRESS> {
 
     private var isExecutionLock: Boolean = false
+    private val any = Any()
 
     /**
      * Constructor
@@ -21,28 +22,29 @@ abstract class BaseBlockAsyncTask<MKR, PROGRESS> : BaseAsyncTask<MKR, PROGRESS> 
     /**
      * Method to set the value of lock
      */
-    @Synchronized
     protected fun setLock(isExecutionLock: Boolean) {
-        this.isExecutionLock = isExecutionLock
+        synchronized(this) {
+            this.isExecutionLock = isExecutionLock
+        }
     }
 
     /**
      * Method to check weather the isExecutionLock is enable of disable.
      */
-    @Synchronized
     protected fun isTaskLock(): Boolean {
-        return isExecutionLock
+        synchronized(this) {
+            return isExecutionLock
+        }
     }
 
     /**
      * Function used to block the call. User must call [setLock] TRUE before calling this method.
      * It return only when task is [setLock] FALSE
      */
-    @Synchronized
     protected fun loopLock() {
         while (isTaskLock()) {
             try {
-                Thread.sleep(50)
+                Thread.sleep(20)
             } catch (e: Exception) {
 
             }
